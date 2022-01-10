f2662a287cc3c7c01f2aee20fcc045a5c7e0adc3
Zahra Mumtaz <zahramumtaz@MP17-ZahraM-SrSE-396.local>
2022-01-10 11:58:53 +0500

    async await

--- day_3/promise.js
+++ day_3/promise.js
@@ -4,12 +4,22 @@ const dos = async() => {
 }
 const tres = () => {return "I am three"};
 
+const dos_1 = () => {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve("I am two");
+        },3000);
+    })
+}
 
-const callMe = () => {
+
+const callMe = async() => {
     let one = uno();
     console.log(one);
-    let two = dos();
+    let two =  dos();
     console.log(two);
+    let two_1 = await dos_1();
+    console.log(two_1);
     let three = tres();
     console.log(three);
 }
